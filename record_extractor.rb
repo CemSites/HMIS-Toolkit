a062a941e12e53121d04d1ab2d7042977c698827
Paul Scarrone <paul.scarrone@gmail.com>
2016-08-20 00:32:25 -0400

    purge characters below 20 as they do not contain data

--- record_extractor.rb
+++ record_extractor.rb
@@ -4,9 +4,14 @@ require_relative './dat_parser.rb'
 encoding_options = {
   :invalid           => :replace,  # Replace invalid byte sequences
   :undef             => :replace,  # Replace anything not defined in ASCII
-  :replace           => '',        # Use a blank for those replacements
+  :replace           => ' ',       # Use a blank for those replacements
   :universal_newline => true       # Always break lines with \n
 }
+
+to_clear = [
+  '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '0a', '0b', '0c', '0d', '0e', '0f',
+  '10', '11', '12', '13', '14'
+]
 
 #parser = DatParser.new '../HELPIC.DAT'
 parser = DatParser.new '../CS00/OWNFIL00.DAT'
@@ -18,13 +23,14 @@ letters.each_with_index do |l, i|
   end
 end
 
-uncompressed_letters = letters.join(' ').to_byte_string
+to_clear_regex = /(#{to_clear.join('|')})/
 
-records = uncompressed_letters.split('RECORD')
-organzied_records = []
+uncompressed_letters = letters.join(' ')
+  .gsub(to_clear_regex, '')
+  .to_byte_string
 
 File.open('OWNFIL00.RECORDS.TXT', 'w') do |file|
-  records.each do |v|
-    file.write("#{v} \n\n")
-  end
+  #records.each do |v|
+    file.write("#{uncompressed_letters.encode('ASCII-8BIT', encoding_options)} \n\n\n\n")
+  #end
 end
